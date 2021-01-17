fc46c1d5df7dea8aa57c102a4815a2e0d639adfe
Arseniy Kouzmenkov <arseniy.kouzmenkov@gmail.com>
2021-01-16 21:33:49 -0700

    Finished this borked mess

--- backend/SolverTest.py
+++ backend/SolverTest.py
@@ -6,7 +6,7 @@ courses = {0:"X1", 1:"X2", 2:"X4", 3:"Y2", 4:"Y3", 5:"Z5"}
 # Course prereq DDG
 coursePreReqGraph = np.array([[0,0,0,0,0,0], [0,0,0,0,0,0], [1,1,0,0,0,0], [0,0,0,0,0,0], [0,1,0,1,0,0], [0,0,1,0,1,0]])
 # What courses are available after completing the courses in this sem
-nextAvailCoursesMat = np.array([[0,0,0.5,0,0,0], [0,0,0.5,0,0.5,0], [0,0,0,0,0,0.5], [0,0,0,0,0.5,0], [0,0,0,0,0,0.5], [0,0,0,0,0,0]])
+nextAvailCoursesMat = np.array([[0,0,2,0,0,0], [0,0,2,0,2,0], [0,0,0,0,0,2], [0,0,0,0,2,0], [0,0,0,0,0,2], [0,0,0,0,0,0]])
 print(coursePreReqGraph)
 print(nextAvailCoursesMat)
 print(nextAvailCoursesMat + coursePreReqGraph.T)
@@ -20,14 +20,28 @@ cprg = coursePreReqGraph
 nacm = nextAvailCoursesMat
 ctt = coursesToTake
 
+print( nacm, ctt, timeConflictGraph)
+
 # Vector of results
 partialSchedules = []
 
-while np.sum(ctt) != 0:
-    # Create a binary vector of the schedule to find through optimization
-    currSched = cp.Variable(6, boolean=True)
-    coursesLeft = cp.Variable(ctt)
-    nextSem = cp.Variable(nacm)
-    tcg = cp.Variable(timeConflictGraph)
+# while np.sum(ctt) != 0:
+# Create a binary vector of the schedule to find through optimization
+currSched = cp.Variable(6, boolean=True)
+#coursesLeft = cp.Variable(ctt)
+#nextSem = cp.Variable(nacm)
+#tcg = cp.Variable(timeConflictGraph)
+tmp = np.array([[1,0,0,0,0,0], [0,1,0,0,0,0], [0,0,0,0,0,0], [0,0,0,1,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]])
+constraints = []
+constraints += [timeConflictGraph @ currSched == 0]
+constraints += [cp.sum(currSched) <= 2]
+#constraints += [1 <= cp.sum(nacm @ currSched)]
 
+objective = cp.Maximize( cp.sum( currSched - ctt ) + cp.sum(nacm @ currSched) )
+
+problem = cp.Problem( objective, constraints )
+problem.solve(solver=cp.GLPK_MI)
+curSched = currSched.value
+
+print( curSched )
 
